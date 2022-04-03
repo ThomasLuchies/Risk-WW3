9f72c2d8f8c9ccd5028f0af976e5080b12112111
qwertyoriuop <41389168+qwertyoriuop@users.noreply.github.com>
2022-04-03 12:16:26 +0200

    comments/ optim

--- Risk/src/Risk/Core/States/Weather.java
+++ Risk/src/Risk/Core/States/Weather.java
@@ -18,6 +18,9 @@ public abstract class Weather
         this.previousWeather = previousWeather;
     }
 
+    /**
+     * changes the state of weather to the next random state
+     */
     public void nextState()
     {
         Weather randomWeather = getRandWeather(this);
@@ -28,6 +31,10 @@ public abstract class Weather
         this.bord.changeState(randomWeather);
     }
 
+    /**
+     * changes the state of weather to the previous state
+     * if there is no previous state it will remain the same state
+     */
     public void previousState()
     {
         if(this.previousWeather == null)
@@ -36,14 +43,24 @@ public abstract class Weather
         this.bord.changeState(previousWeather);
     }
 
+    /**
+     * gets weather and changes this to a random weather state
+     * @param currentWeather the current value
+     * @return a random weather state
+     */
     public Weather getRandWeather(Weather currentWeather)
     {
         Weather[] weatherStates = new Weather[] { new NormalWeather(this.bord,this), new ColdWeather(this.bord, this), new WarmWeather(this.bord, this) };
         Random random = new Random();
-        int randInt = random.nextInt(3) + 1;
+        //int randInt = random.nextInt(3);
 
-        return weatherStates[randInt];
+        return weatherStates[random.nextInt(3)];
     }
 
+    /**
+     * checks what type of country the supplied country is and uses the corresponding dice
+     * @param country
+     * @return correct dice funtion DiceNormal()/DiceAdvantage()/DiceDisdvantage()
+     */
     public abstract Dice getCorrectDice(Country country);
 }
